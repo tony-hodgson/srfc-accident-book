9cc5cb80b954ca3e95141ebcd37f78a9b8cd5275
tony-hodgson <hodgson.tony@gmail.com>
2026-01-20 14:37:57 +0000

    Change to netlify config

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './app/interceptors/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
